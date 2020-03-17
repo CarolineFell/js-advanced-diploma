2d8876eedf88121b8bdfcb35529e638a5b6af09a
Linn <carolinefell@yandex.ru>
2020-03-17 09:53:24 +0300

    utils

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -1,12 +1,57 @@
+import GamePlay from './GamePlay';
+import themes from './themes';
+import GameState from './GameState';
+import utils from './utils';
+
 export default class GameController {
   constructor(gamePlay, stateService) {
     this.gamePlay = gamePlay;
     this.stateService = stateService;
+    this.gameState = new GameState();
   }
 
   init() {
     // TODO: add event listeners to gamePlay events
     // TODO: load saved stated from stateService
+
+    this.gamePlay.drawUi(this.gameState.theme);
+
+  //     /**
+  //  * Add listener to mouse click for cell
+  //  *
+  //  * @param callback
+  //  */
+  // addCellClickListener(callback) {
+  //   this.cellClickListeners.push(callback);
+  // }
+
+  // /**
+  //  * Add listener to "New Game" button click
+  //  *
+  //  * @param callback
+  //  */
+  // addNewGameListener(callback) {
+  //   this.newGameListeners.push(callback);
+  // }
+
+  // /**
+  //  * Add listener to "Save Game" button click
+  //  *
+  //  * @param callback
+  //  */
+  // addSaveGameListener(callback) {
+  //   this.saveGameListeners.push(callback);
+  // }
+
+  // /**
+  //  * Add listener to "Load Game" button click
+  //  *
+  //  * @param callback
+  //  */
+  // addLoadGameListener(callback) {
+  //   this.loadGameListeners.push(callback);
+  // }
+
   }
 
   onCellClick(index) {
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,6 +1,34 @@
 export function calcTileType(index, boardSize) {
-  // TODO: write logic here
-  return 'center';
+  const topLeft = 0; // begins with 0
+  const topRight = boardSize - 1; // 4 - 1 = 3
+  const bottomLeft = boardSize * (boardSize - 1); // 4 * (4 - 1) = 12
+  const bottomRight = boardSize ** 2 - 1; // 4 ** 2 - 1 = 15
+
+  // boardSize = 4 --> (4x4)
+  // 0  1  2  3 
+  // 4  5  6  7
+  // 8  9  10 11
+  // 12 13 14 15
+
+  if (index === topLeft) {
+    return 'top-left';
+  } else if (index === topRight) {
+    return 'top-right';
+  } else if (index === bottomLeft) {
+    return 'bottom-left';
+  } else if (index === bottomRight) {
+    return 'bottom-right';
+  } else if (index > topLeft && index < topRight) { // 1 || 2
+    return 'top';
+  } else if (index > bottomLeft && index < bottomRight) { // 13 || 14
+    return 'bottom';
+  } else if (index % boardSize === 0) { // 4 % 4 === 0 || 8 % 4 === 0
+    return 'left';
+  } else if ((index + 1) % boardSize === 0) { // (7 + 1) % 4 === 0 || (11 + 1) % 4 === 0
+    return 'right';
+  } else { // 5 || 6 || 9 || 10
+    return 'center';
+  }
 }
 
 export function calcHealthLevel(health) {
